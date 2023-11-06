06c82433df7317d15f0d2771bb7b34642d7757ad
ranveersingh <ranveersingh@ranveers-air.lan>
2023-11-06 08:30:45 -0500

    Analyze problem

---
+++ GarmentSimulator/src/main/java/com/mycompany/garmentsimulator/GarmentSimulator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.garmentsimulator;
+
+/**
+ *
+ * @author ranveersingh
+ */
+public class GarmentSimulator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
